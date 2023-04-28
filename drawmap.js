2fcdb90119a309776a3dba4a153494f48043b066
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-04-28 16:17:34 -0400

    Update drawmap.js

--- drawmap.js
+++ drawmap.js
@@ -1,12 +1,12 @@
 function drawMap() {
     // Double forloop to draw
-    for (let y = array2d.length[0] - 1; y >=0 ; y--) {
-        for (let x = 0; x < array2d.length; x++) {
+    for (let x = array2d.length - 1; x >= 0 ; x--) {
+        for (let y = 0; x < array2d[0].length; y++) {
             if (array2d[x][y] >= THRESHOLD) {
                 // Draw the rectangle
                 cvs.fillStyle = 'rgba(255, 255, 255)';
-                cvs.fillRect(x, y, 1, 1);
-               // cvs.fillRect(y, x, 1, 1);
+                //cvs.fillRect(x, y, 1, 1);
+                cvs.fillRect(y, x, 1, 1);
                 // Phai rotate cai distance field de no ko anh huong toi clicking particle
             }
         }
