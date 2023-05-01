070a728fbc656c65d84e7dd82b9ff80621fa831e
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-05-01 10:50:38 -0400

    Update drawmap.js

--- drawmap.js
+++ drawmap.js
@@ -18,16 +18,16 @@ function drawMap() {
     //var n = 10;
     var n = array2d.length;
     var m = array2d[0].length;
-    for (let i = 0; i < n; i++) 
+    for (let i = 0; i < m; i++) 
     {
-        for (let j = 0; j < m; j++) 
+        for (let j = 0; j < n; j++) 
         {
             array[i][j] = array2d[j][i];
         }
     }
-    for (let x = array.length - 1; x >= 0 ; x--) {
+    for (let x = 0; x < array.length ; x++) {
         for (let y = 0; y < array[0].length; y++) {
-            if (array2d[x][y] >= THRESHOLD) {
+            if (array[x][y] >= THRESHOLD) {
                 // Draw the rectangle
                 cvs.fillStyle = 'rgba(255, 255, 255)';
                 cvs.fillRect(x, y, 1, 1);
