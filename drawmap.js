d8d9b8c80311b00f25358dcabb08ca768c1bfe25
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-05-01 00:58:37 -0400

    Update drawmap.js

--- drawmap.js
+++ drawmap.js
@@ -21,7 +21,7 @@ function drawMap() {
             if (array2d[x][y] >= n) {
                 // Draw the rectangle
                 cvs.fillStyle = 'rgba(255, 255, 255)';
-                cvs.fillRect(x, y, 1, 1);
+                cvs.fillRect(x, y, 2, 2);
                 //cvs.fillRect(y, x, 1, 1);
             }
         }
