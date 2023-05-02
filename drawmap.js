d81849e3804d0b0cbc315c5a64d99fb2813f1b41
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-05-01 22:46:23 -0400

    Update drawmap.js

--- drawmap.js
+++ drawmap.js
@@ -16,8 +16,8 @@
 function drawMap() {
     // Double forloop to draw
     var n = 10;
-    for (let x = 0; x < array2d.length; x++) {
-        for (let y = 0; y < array2d[0].length; y++) {
+    for (let x = 0; x < array.length; x++) {
+        for (let y = 0; y < array[0].length; y++) {
             if (array2d[x][y] >= n) {  
                 cvs.fillStyle = 'rgba(255, 255, 255)';
                 cvs.fillRect(x, y, 1, 1);
