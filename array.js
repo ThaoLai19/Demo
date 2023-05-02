5afa7c458e8f1029c358d4551ec63e1ca3d68a8b
Thao Lai <83465146+ThaoLai19@users.noreply.github.com>
2023-05-02 17:01:26 -0400

    Update array.js

--- array.js
+++ array.js
@@ -1,19 +1,4 @@
-/*function drawMap() {
-    // Double forloop to draw
-    for (let x = array2d.length - 1; x >= 0 ; x--) {
-        for (let y = 0; y < array2d[0].length; y++) {
-            if (array2d[x][y] >= THRESHOLD) {
-                // Draw the rectangle
-                cvs.fillStyle = 'rgba(255, 255, 255)';
-                //cvs.fillRect(x, y, 1, 1);
-                cvs.fillRect(y, x, 1, 1);
-                // Phai rotate cai distance field de no ko anh huong toi clicking particle
-            }
-        }
-    }
-}*/
-
-function drawMap() {  
+function getArray() {  
     // Double forloop to draw
     //var n = 10;
     const rows = array1[0].length;
@@ -28,26 +13,4 @@ function drawMap() {
           array2d[i][j] = array1[j][i];  
         }
       }
-    for (let x = 0; x < array2d.length; x++) {
-        for (let y = 0; y < array2d[0].length; y++) {
-            if (array2d[x][y] >= THRESHOLD) {  
-                cvs.fillStyle = 'rgba(255, 255, 255)';
-                cvs.fillRect(x, y, 1, 1);
-            }
-        }
-    }
 }
-
-/*function drawMap() {
-    // Double forloop to draw
-    for (let x = 0; x < array2d.length ; x++) {
-        for (let y = 0; y < array2d[0].length; y++) {
-            if (array2d[x][y] >= THRESHOLD) {
-                // Draw the rectangle
-                cvs.fillStyle = 'rgba(255, 255, 255)';
-                cvs.fillRect(x, y, 1, 1);
-                //cvs.fillRect(y, x, 1, 1);
-            }
-        }
-    }
-}*/
